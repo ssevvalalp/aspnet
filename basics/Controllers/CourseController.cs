daf0c2fbdefc0d932e9dbfe46b3890398bcfa716
sevvala <sevvala@SEVVALALP>
2024-01-25 16:28:48 +0300

    Add project files.

---
+++ basics/Controllers/CourseController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace basics.Controllers
+{
+    //localhost -->home/index
+    //localhost/home  -->home/index
+    //localhost/home/index -->home/index
+
+    //course
+    public class CourseController : Controller
+    {
+        //course
+        //course/index
+        public string Index()
+        {
+            return "course/index";
+        }
+
+        public string List()
+        {
+            return "course/list";
+        }
+    }
+}
---
+++ basics/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using basics.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Diagnostics;
+
+namespace basics.Controllers
+{
+    public class HomeController : Controller
+    {
+      
+
+        public string Index()
+        {
+            return "home/index";
+        }
+
+        public string Contact()
+        {
+            return "home/Contact";
+        }
+
+    }
+}
