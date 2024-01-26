0276cd95a121653ae7b2eb51648adf9c1de5daf3
Şevval Alp <ssevvalalp@gmail.com>
2024-01-26 14:25:23 +0300

    Partial views added

---
+++ basics/Views/Course/_CourseCard.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace basics.Views.Course
+{
+    public class _CourseCardModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
