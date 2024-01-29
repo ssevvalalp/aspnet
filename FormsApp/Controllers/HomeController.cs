069abe0e1fc2c55ad0b04ff348c6004277aab2c1
Şevval Alp <ssevvalalp@gmail.com>
2024-01-29 14:02:34 +0300

    QueryString

--- FormsApp/Controllers/HomeController.cs
+++ FormsApp/Controllers/HomeController.cs
@@ -12,9 +12,16 @@ namespace FormsApp.Controllers
             
         }
 
-        public IActionResult Index()
+        public IActionResult Index(string searchString)
         {
-            return View(Repository.Products);
+            var products = Repository.Products;
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                products = products.Where(p => p.Name.ToLower().Contains(searchString)).ToList();
+                
+            }
+            return View(products);
+
         }
 
         public IActionResult Privacy()
