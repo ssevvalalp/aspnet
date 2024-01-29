6e21c034943d05ca1c40b13ea976c712e9be9eae
Şevval Alp <ssevvalalp@gmail.com>
2024-01-29 15:28:41 +0300

    Categories Select

--- FormsApp/Controllers/HomeController.cs
+++ FormsApp/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using FormsApp.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using System.Diagnostics;
 
 namespace FormsApp.Controllers
@@ -12,15 +13,23 @@ namespace FormsApp.Controllers
             
         }
 
-        public IActionResult Index(string searchString)
+        public IActionResult Index(string searchString, string category)
         {
             var products = Repository.Products;
+
             if (!String.IsNullOrEmpty(searchString))
             {
                 ViewBag.SearchString = searchString;
                 products = products.Where(p => p.Name.ToLower().Contains(searchString)).ToList();
                 
             }
+
+            if (!String.IsNullOrEmpty(category) && category != "0")
+            {
+                products = products.Where(p => p.CategoryId == int.Parse(category)).ToList();
+            }
+
+                ViewBag.Categories = new SelectList(Repository.Categories, "CategoryId", "Name", category);
             return View(products);
 
         }
