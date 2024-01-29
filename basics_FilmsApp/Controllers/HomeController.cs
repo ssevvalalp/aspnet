9982f6e6a79074a46874f88e28f03920c11db0cf
Şevval Alp <ssevvalalp@gmail.com>
2024-01-29 15:59:50 +0300

    Films App Created

---
+++ basics_FilmsApp/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using basics_FilmsApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace basics_FilmsApp.Controllers
+{
+    public class HomeController : Controller
+    {
+       
+
+        public HomeController()
+        {
+            
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+    }
+}
