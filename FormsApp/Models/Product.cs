fae81132e45730254a3af20dff8452b41cd07d3c
Şevval Alp <ssevvalalp@gmail.com>
2024-01-29 13:47:51 +0300

    Product List

--- FormsApp/Models/Product.cs
+++ FormsApp/Models/Product.cs
@@ -1,16 +1,20 @@
-﻿namespace FormsApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FormsApp.Models
 {
     public class Product
     {
+        [Display(Name = "Product Id")]
         public int ProductId { get; set; }
 
         public string Name { get; set; } = string.Empty;
 
+        [Display(Name = "Price")]
         public decimal Price { get; set; }
 
         public string? Image { get; set; }
 
-        public bool IsAcvtive { get; set; }
+        public bool IsActive { get; set; }
 
         public int CategoryId { get; set; } //foreign key
     }
